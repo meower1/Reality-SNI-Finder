76a1f99ef7e0204f22aba92739cb80a5044be7ac
meower1 <109747197+meower1@users.noreply.github.com>
2023-10-16 22:12:00 +0330

    Update v1.1 now lists the sni's based on tlsping in a more organized format

--- main.py
+++ main.py
@@ -1,45 +1,40 @@
-import os
-import subprocess
-import re
+from subprocess import check_output, run, CalledProcessError
+from re import findall
+from pandas import DataFrame
+from tabulate import tabulate
 
+# Read the sni.txt file and split it into a list of domain names
+with open("sni.txt", "r") as my_file:
+    data = my_file.read()
+    sni_list = data.split("\n")
+
+# Remove any empty strings from the sni_list
+sni_list = list(filter(None, sni_list))
+
+# Test all the domains in sni.txt file and put the results in a list called result
 result = []
+try:
+    for i in sni_list:
+        x = check_output(f"./tlsping {i}:443", shell=True).rstrip().decode('utf-8')
+        result.append(x)
+except CalledProcessError:
+    pass
+
+# Extract all the avg tlsping values from the domains
 avg_value_list = []
-domain_ping_dict = {}
-
-my_file = open("sni.txt", "r")
-
-data = my_file.read()
-
-# replacing end splitting the text when newline ('\n') is seen.
-sni_list = data.split("\n")
-my_file.close()
-
-#this tests all the domains in sni.txt file and puts them in a list called result
-
-for i in sni_list:
-    x = subprocess.check_output(f"./tlsping {i}:443", shell=True).rstrip().decode('utf-8')
-    result.append(x)
-    
-#this extracts all the avg tlsping values from the domains
-
 for j in result:
-# use regular expressions to extract the "avg" value
-    avg_value = re.findall(r"avg/.*?ms.*?(\d+\.?\d*)ms", j )[0]
+    # Use regular expressions to extract the "avg" value
+    avg_value = findall(r"avg/.*?ms.*?(\d+\.?\d*)ms", j )[0]
     avg_value_list.append(avg_value)
 
-# this puts the sni_list values inside domain_ping_dict as keys and the avg_value_list values as values
-print(avg_value_list)
+# Create a dictionary with the domain names as keys and the avg values as values
 domain_ping_dict = {sni_list[i]: float(avg_value_list[i]) for i in range(len(sni_list))}
 
-#this sorts the dictionary by the values in ascending order
+# Sort the dictionary by the values in ascending order
 sorted_dict = dict(sorted(domain_ping_dict.items(), key=lambda item: item[1]))
 
-#final result :)
-best_sni = list(sorted_dict.keys())[0]
-print(best_sni)
-
-os.system("clear")
-print("Best SNI is : " + best_sni)
-print("\nYou can manaully set this as (dest) and (sni) in your xray config file.")
-print("Have a good day o/.\n")
+# Convert the sorted dictionary to a pandas DataFrame and print it using tabulate
+df = DataFrame(sorted_dict.items(), columns=['domains', 'pings(ms)'])
+run('clear')
+print(tabulate(df, headers='keys', tablefmt='psql'))
 
